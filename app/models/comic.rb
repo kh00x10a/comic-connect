37c59a16ced5e0fe71fd475d00afdd4fd3a1485d
kh00x10a <sein.nicht04@gmail.com>
2023-06-11 19:13:17 +0900

    モデルのバリデーションの修正

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -4,9 +4,12 @@ class Comic < ApplicationRecord
   belongs_to :magazine
   belongs_to :decade
 
-  validates :title, :author, : presence: true
+  validates :title, :author, presence: true
 
   validates :genre_id, :magazine_id, :decade_id, numericality: { other_than: 1 , message:"can't be blank"}
-  
+
   belongs_to :user
+  has_one_attached :image
+
+
 end
