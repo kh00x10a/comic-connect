a3c577199440d42cd5fc273481c26a5f0acd9774
kh00x10a <sein.nicht04@gmail.com>
2023-06-12 18:21:04 +0900

    コントローラーの編集

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,6 @@
 class ComicsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_comic, except: [:index, :new, :create]
   def index
     @comics = Comic.order("created_at DESC")
   end
@@ -19,18 +19,15 @@ class ComicsController < ApplicationController
   end
 
   def show
-    @comic = Comic.find(params[:id])
   end
 
   def edit
-    @comic = Comic.find(params[:id])
     if current_user != @comic.user
       redirect_to root_path
     end
   end
 
   def update
-    @comic = Comic.find(params[:id])
     @comic.update(comic_params)
     if @comic.update(comic_params)
       redirect_to comic_path(@comic)
@@ -39,9 +36,22 @@ class ComicsController < ApplicationController
     end
   end
 
+  def destroy
+    if user_signed_in? && current_user == @comic.user
+      @comic.destroy
+    end
+    if @comic.destroy
+      redirect_to root_path
+    end
+  end
+
   private
 
   def comic_params
     params.require(:comic).permit(:title, :author, :illustrator, :genre_id, :magazine_id, :decade_id, :publisher, :publication, :impression, :image).merge(user_id: current_user.id)
+  end
+
+  def set_comic
+    @comic = Comic.find(params[:id])
   end
 end
