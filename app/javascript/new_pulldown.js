556db04b82676fd58f8d8a04bb477a6b710e81ea
kh00x10a <sein.nicht04@gmail.com>
2023-06-20 17:58:48 +0900

    ビューの編集

--- app/javascript/new_pulldown.js
+++ app/javascript/new_pulldown.js
@@ -15,11 +15,11 @@ function pullDown() {
 
   pullDownChild.forEach(function(element) {
     element.addEventListener('mouseover', function() {
-      this.style.color = "#FF0000"; // カーソルを置いた時の色を設定
+      this.style.color = "green"; // カーソルを置いた時の色を設定
     });
 
     element.addEventListener('mouseout', function() {
-      this.style.color = ""; // デフォルトの色に戻す場合は空文字列にする
+      this.style.color = "#FF0000"; // デフォルトの色に戻す場合は空文字列にする
     });
   });
 }
